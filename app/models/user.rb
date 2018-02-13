65827afe32aea794e264f8a528dcbae49b86108f
Nicholas Williams <nawilliams@umass.edu>
2018-02-13 17:33:16 -0500

    user has_many subordinates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     belongs_to :position
     belongs_to :supervisor, foreign_key: :supervisor_id, class_name: 'User', optional: true
+    has_many :subordinates, foreign_key: :supervisor_id, class_name: 'User'
 end
