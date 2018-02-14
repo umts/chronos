2c471e9d711b7c7da194265d8dc04522c352f7dc
Nicholas Williams <nawilliams@umass.edu>
2018-02-14 15:16:51 -0500

    verifyin time being in the past prevents deit=

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -6,7 +6,5 @@ class Shift < ApplicationRecord
 
   def verify_times
     errors.add(:end_time, 'End time cannot be before start time') if start_time < end_time
-    errors.add(:start_time, 'Start time cannot be in the past')   if start_time < Time.zone.now
-    errors.add(:end_time, 'End time cannot be in the past')       if end_time < Time.zone.now
   end
 end
