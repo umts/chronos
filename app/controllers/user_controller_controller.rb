bdc434970845e084fb44839516264aefbbf75adc
Nicholas Williams <nawilliams@umass.edu>
2018-02-21 13:37:54 -0500

    create and update

--- app/controllers/user_controller_controller.rb
+++ app/controllers/user_controller_controller.rb
@@ -2,21 +2,41 @@ class UserControllerController < ApplicationController
   def index
   end
 
-  def create
-  end
-
-  def new
-  end
-
-  def  edit
-  end
-
   def show
   end
 
-  def update
+  def new
+    @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'User successfully created'
+      redirect_to users_path
+    else
+      flash[:warning] = @user.errors.full_messages
+      render 'new'
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'User successfully updated'
+      redirect_to users_path
+    else
+      flash[:warning] = 'User could now be updated'
+      render 'edit'
+    end
+
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit()
   end
 end
