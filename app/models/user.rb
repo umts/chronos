8f7406dc4d847f8612d5af1fe68ae17553a3d7e4
Nicholas Williams <nawilliams@umass.edu>
2018-02-26 13:21:44 -0500

    basic user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-    belongs_to :position
-    belongs_to :supervisor, foreign_key: :supervisor_id, class_name: 'User', optional: true
-    has_many :subordinates, foreign_key: :supervisor_id, class_name: 'User'
-    has_many :shifts
+  belongs_to :position
+  belongs_to :supervisor, foreign_key: :supervisor_id, class_name: 'User', optional: true
+  has_many :subordinates, foreign_key: :supervisor_id, class_name: 'User'
+  has_many :shifts
+
+  validates :first_name, :last_name, :email, :spire_id, presence: true
+  validates :spire_id,  uniqueness: true,
+                        length: { is: 8 },
+                        numericality: { only_integer: true }
 end
