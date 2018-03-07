bbf947addff30ba72a4f7d1e02c5be96180ae850
Nicholas Williams <nawilliams@umass.edu>
2018-03-07 17:08:22 -0500

    haml for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,9 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    @supervisors = {} # placeholders
+    @divisions = {}
+    @positions = {}
   end
 
   def create
