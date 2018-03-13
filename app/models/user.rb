1993e03fb2b95f8ac8c630e9d3394c45de775f4b
Nicholas Williams <nawilliams@umass.edu>
2018-03-12 23:30:07 -0400

    add division to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   belongs_to :position
+  belongs_to :division
   belongs_to :supervisor, foreign_key: :supervisor_id, class_name: 'User', optional: true
   has_many :subordinates, foreign_key: :supervisor_id, class_name: 'User'
   has_many :shifts
