f88481368582c95b25c5358e6868e0a8ea87b108
Nicholas Williams <nawilliams@umass.edu>
2018-03-27 17:50:50 -0400

    dependent destroy/nullify and scope for user

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,5 +1,5 @@
 class Division< ApplicationRecord
-  has_many :users
+  has_many :users, dependent: :nullify
 
   validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,14 @@ class User < ApplicationRecord
   belongs_to :division
   belongs_to :supervisor, foreign_key: :supervisor_id, class_name: 'User', optional: true
   has_many :subordinates, foreign_key: :supervisor_id, class_name: 'User'
-  has_many :shifts
+  has_many :shifts, dependent: :destroy
 
   validates :first_name, :last_name, :email, :spire_id, presence: true
   validates :spire_id,  uniqueness: true,
                         length: { is: 8 },
                         numericality: { only_integer: true }
+
+  scope :active, -> { where(active: true) }
+  scope :inactive, -> { where(active: false) }
+  
 end
