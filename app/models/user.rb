21e27fc7fdfab532c90ff65a7a2a43c193f4da2c
Nicholas Williams <nawilliams@umass.edu>
2018-03-28 14:34:08 -0400

    correctly use include blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  belongs_to :position
+  belongs_to :position, optional: true
   belongs_to :division, optional: true
   belongs_to :supervisor, foreign_key: :supervisor_id, class_name: 'User', optional: true
   has_many :subordinates, foreign_key: :supervisor_id, class_name: 'User'
