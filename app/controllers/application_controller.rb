10b250641800ea0f7f050f1a37655485e24a91af
Nicholas Williams <nawilliams@umass.edu>
2018-03-30 18:43:47 -0400

    set current_user from shibboleth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :set_current_user
+  # TODO: before_action redirect to timeclock page when swipe_id is in session after timeout
+
   # paper trail
   before_action :set_paper_trail_whodunnit
+
+  private
+
+  def set_current_user
+    @current_user =
+      if session.key? :swipe_id
+        User.active.find_by(swipe_id: session[:swipe_id])
+      elsif session.key? :user_id
+        User.active.find_by(id: sesison[:user_id])
+      elsif request.env['fcIdNumber']
+        user = User.active.find_by(spire_id: request.env['fcIdNumber'])
+        session[:user_id] = user.id
+        user
+      end
+  end
 end
