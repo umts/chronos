b4a3af2dcb859b6d51a569a236867f2768a9813b
Nicholas Williams <nawilliams@umass.edu>
2018-04-02 18:36:16 -0400

    forgot to set @user on update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,7 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = 'User Successfully Updated'
       redirect_to users_path
