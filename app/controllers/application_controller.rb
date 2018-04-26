4887272e7cefc75ad9126bec9498e27a4a98beaa
Nicholas Williams <nawilliams@umass.edu>
2018-04-26 18:33:07 -0400

    sesison

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_current_user
-  # TODO: before_action redirect to timeclock page when swipe_id is in session after timeout
 
   # paper trail
   before_action :set_paper_trail_whodunnit
@@ -11,11 +10,11 @@ class ApplicationController < ActionController::Base
 
   def set_current_user
     @current_user =
-      if session.key? :swipe_id
+      if session.key? :swipe_id # swipe in at timeclock
         User.active.find_by(swipe_id: session[:swipe_id])
       elsif session.key? :user_id
-        User.active.find_by(id: sesison[:user_id])
-      elsif request.env['fcIdNumber']
+        User.active.find_by(id: session[:user_id])
+      elsif request.env['fcIdNumber'] # shibboleth
         user = User.active.find_by(spire_id: request.env['fcIdNumber'])
         session[:user_id] = user.id
         user
