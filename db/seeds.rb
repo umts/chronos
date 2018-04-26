82bfd1ee3e39d4fe752d668b8de7436113cf5264
Nicholas Williams <nawilliams@umass.edu>
2018-04-26 19:01:45 -0400

    seeds for user, position, division, union

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env.development?
+  union1 = Union.create(name: 'Test Union 1')
+  union2 = Union.create(name: 'Test Union 2')
+
+  division1 = Division.create(name: 'Test Division 1')
+  division2 = Division.create(name: 'Test Division 2')
+
+  position1 = Position.create(name: 'Test Position 1', union: union1)
+  position2 = Position.create(name: 'Test Position 2', union: union2)
+
+  supervisor = User.create(first_name: 'Test', last_name: 'Supervisor',
+                           email: 'supervisor@test.com',
+                           spire_id: '00000001',
+                           position: position1,
+                           division: division1,
+                           is_supervisor: true)
+  user1 = User.create(first_name: 'Test', last_name: 'User 1',
+                      email: 'user1@test.com',
+                      spire_id: '00000002',
+                      position: position1,
+                      division: division2,
+                      supervisor: supervisor)
+  user2 = User.create(first_name: 'Test', last_name: 'User 2',
+                      email: 'user2@test.com',
+                      spire_id: '00000003',
+                      position: position2,
+                      division: division1,
+                      supervisor: supervisor)
+end
