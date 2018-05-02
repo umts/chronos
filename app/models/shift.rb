51fdfc98370f708ff420f2a4b578fd8dc983a9db
Nicholas Williams <nawilliams@umass.edu>
2018-05-02 14:36:46 -0400

    remove time validation, it gets implemented properly in another branch

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -3,8 +3,4 @@ class Shift < ApplicationRecord
   belongs_to :user
 
   validate :verify_times
-
-  def verify_times
-    errors.add(:end_time, 'End time cannot be before start time') if start_time < end_time
-  end
 end
