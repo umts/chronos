48deb0e76b84a8f7f9073d85a8f82a04665c74f4
Nicholas Williams <nawilliams@umass.edu>
2018-05-02 18:54:20 -0400

    user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,15 @@ RSpec.describe User, type: :model do
     @user = create :user
   end
 
-  it 'works' do
-    expect(@user.first_name).to be('Test')
+  describe 'full_name' do
+    it 'returns users full name' do
+      expect(@user.full_name).to eq('Test User')
+    end
+  end
+
+  describe 'union' do
+    it 'returns the users union' do
+      expect(@user.union.name).to eq('Test Union')
+    end
   end
 end
