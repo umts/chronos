8f5f2accdeb7909a91e1d330a46178f47ea78dcb
Nicholas Williams <nawilliams@umass.edu>
2018-05-09 12:51:25 -0400

    set default constants

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,1 +1,11 @@
-CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+begin
+  CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+rescue Errno::ENOENT
+  CONFIG = {}
+end
+
+CONFIG.reverse_merge(
+  lunch_start: 6,
+  lunch_duration: 0.5
+)
+
