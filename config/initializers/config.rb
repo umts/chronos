2f4f47705f6422e854e6b9ab7d664b208a9ec75b
Nicholas Williams <nawilliams@umass.edu>
2018-05-09 13:15:44 -0400

    fix config hash, yml keys get loaded as strings

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -4,9 +4,9 @@ rescue Errno::ENOENT
   CONFIG = {}
 end
 
-CONFIG.reverse_merge(
-  lunch_start: 6,
-  lunch_duration: 0.5
+CONFIG.reverse_merge!(
+  'lunch_start' => 6,
+  'lunch_duration' => 0.5
 )
 
 CONFIG.freeze
